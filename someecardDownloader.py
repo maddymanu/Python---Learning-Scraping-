b321eff49aada51879292ccf9d797d0e3640f34c
Aditya Bansal <maddymanu@hotmail.com>
2014-08-17 16:25:43 +0530

    UPDATE : Prev Not working

--- someecardDownloader.py
+++ someecardDownloader.py
@@ -13,7 +13,7 @@ if not os.path.exists(somecarddir):
         os.makedirs(somecarddir)
 
 
-main_url = "http://www.someecards.com/ecards/flirting/?filter=someecards&sort=classics"
+main_url = "http://www.someecards.com/ecards/flirting/?filter=someecards&sort=popular"
 main_url_opened = urllib.urlopen(main_url)
 main_url_response = main_url_opened.read()
 
@@ -22,10 +22,12 @@ browser.get(main_url)
 time.sleep(1)
 
 elem = browser.find_element_by_tag_name("body")
-no_of_pagedowns = 5
+no_of_pagedowns = 10
 
 
-soup = BeautifulSoup(main_url_response)
+html = browser.page_source
+
+soup = BeautifulSoup(html)
 links = []
 
 #Going through each page
@@ -33,16 +35,18 @@ while no_of_pagedowns:
     elem.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.2)
     no_of_pagedowns-=1
+    html = browser.page_source
+    soup = BeautifulSoup(html)
     for somecard_link in soup.find_all("a"):
       try:
         href_link = somecard_link.get("href")
-        # print href_link #this is printing the href link many times
+        print href_link #this is printing the href link many times
         split_link = href_link.split("/")
         try:
-          if split_link[1] == "flirting-cards":
+          if split_link[1] == "flirting-cards" or split_link[1] == "usercards":
             # print split_link[1]
             if href_link not in links:
-              print href_link
+              # print href_link
               links.append(href_link)
         except: pass
       except: pass
